00dffce3eaea50f4e8a004f8ac0bbf26ea31d424
Khoa Bui <khoabuivnhcm@gmail.com>
2019-09-18 15:56:14 +0700

    Solution 1 - function code in TrafficLight

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 import './TrafficLight.css';
 
+var classnames = require('classnames');
+
+const RED = 0;
+const YELLOW = 1;
+const GREEN = 2;
+
 class TrafficLight extends Component {
+    constructor() {
+      super();
+      this.state = {
+        currentColor: RED
+      }
+      setInterval(() => {
+        this.setState({
+          currentColor: this.getNextLight(this.state.currentColor)
+        })
+      }, 1000)
+    }
+    getNextLight(color) {
+      switch (color) {
+        case RED:
+          return YELLOW;
+          break;
+        case YELLOW:
+          return GREEN;
+          break;
+        case GREEN:
+          return RED;
+          break;
+        default:
+          break;
+      }
+      
+    }
     render() {
+        const { currentColor } = this.state;
         return (
            <div className="TrafficLight">
-            <div className="bulb red"></div>
-            <div className="bulb yellow"></div>
-            <div className="bulb green"></div>
+            <div className={classnames("bulb", "red", {
+              active: currentColor === RED
+            })}></div>
+            <div className={classnames("bulb", "yellow", {
+              active: currentColor === YELLOW
+            })}></div>
+            <div className={classnames("bulb", "green", {
+              active: currentColor === GREEN
+            })}></div>
           </div>
         )
     }
